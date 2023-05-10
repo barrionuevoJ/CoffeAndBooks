3b771d035edd6d47d9c737cb93fcebc4c4724e55
BarrionuevoJ <julianbarrionuevo10@gmail.com>
2023-05-10 11:35:42 -0300

    archivo .sql de la base de datos

---
+++ coffeandbooks_db.sql
@@ -0,0 +1,121 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 10-05-2023 a las 16:33:03
+-- Versión del servidor: 10.4.28-MariaDB
+-- Versión de PHP: 8.0.28
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `coffeandbooks_db`
+--
+CREATE DATABASE IF NOT EXISTS `coffeandbooks_db` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+USE `coffeandbooks_db`;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `autores`
+--
+
+DROP TABLE IF EXISTS `autores`;
+CREATE TABLE IF NOT EXISTS `autores` (
+  `id_autor` int(11) NOT NULL AUTO_INCREMENT,
+  `autor` varchar(100) NOT NULL,
+  PRIMARY KEY (`id_autor`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `categorias`
+--
+
+DROP TABLE IF EXISTS `categorias`;
+CREATE TABLE IF NOT EXISTS `categorias` (
+  `id_categoria` int(11) NOT NULL AUTO_INCREMENT,
+  `categoria` varchar(100) NOT NULL,
+  PRIMARY KEY (`id_categoria`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `generos`
+--
+
+DROP TABLE IF EXISTS `generos`;
+CREATE TABLE IF NOT EXISTS `generos` (
+  `id_genero` int(11) NOT NULL AUTO_INCREMENT,
+  `genero` varchar(50) NOT NULL,
+  PRIMARY KEY (`id_genero`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `productos`
+--
+
+DROP TABLE IF EXISTS `productos`;
+CREATE TABLE IF NOT EXISTS `productos` (
+  `id_producto` int(11) NOT NULL AUTO_INCREMENT,
+  `titulo` varchar(100) NOT NULL,
+  `descripcion` text DEFAULT NULL,
+  `cantidad` int(11) DEFAULT 1,
+  `precio` int(11) DEFAULT 1,
+  `img` varchar(255) DEFAULT 'default-image.png',
+  `descuento` int(11) DEFAULT 0,
+  `id_autor` int(11) NOT NULL,
+  `id_genero` int(11) NOT NULL,
+  `id_categoria` int(11) NOT NULL,
+  PRIMARY KEY (`id_producto`),
+  KEY `FK_cdab2f19-6071-4278-b7b9-f7ebb0db7a61` (`id_categoria`),
+  KEY `FK_600de83d-6964-4fe9-bc78-10d4e3d3f46d` (`id_autor`),
+  KEY `FK_f3246963-dae2-4d49-9a0a-7b1fe23d4be4` (`id_genero`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `usuarios`
+--
+
+DROP TABLE IF EXISTS `usuarios`;
+CREATE TABLE IF NOT EXISTS `usuarios` (
+  `id_user` int(11) NOT NULL,
+  `firstName` varchar(100) NOT NULL,
+  `lastName` varchar(100) NOT NULL,
+  `email` varchar(200) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `profileImg` varchar(255) DEFAULT 'default-user.png',
+  PRIMARY KEY (`id_user`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `productos`
+--
+ALTER TABLE `productos`
+  ADD CONSTRAINT `FK_600de83d-6964-4fe9-bc78-10d4e3d3f46d` FOREIGN KEY (`id_autor`) REFERENCES `autores` (`id_autor`),
+  ADD CONSTRAINT `FK_cdab2f19-6071-4278-b7b9-f7ebb0db7a61` FOREIGN KEY (`id_categoria`) REFERENCES `categorias` (`id_categoria`),
+  ADD CONSTRAINT `FK_f3246963-dae2-4d49-9a0a-7b1fe23d4be4` FOREIGN KEY (`id_genero`) REFERENCES `generos` (`id_genero`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
