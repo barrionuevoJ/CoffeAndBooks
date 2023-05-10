8b184bf9c4887aa7784b3e4877be01ba19c6aa12
BarrionuevoJ <julianbarrionuevo10@gmail.com>
2023-05-09 21:28:58 -0300

    Sprint 3

--- app.js
+++ app.js
@@ -1,21 +1,19 @@
 const express = require('express');
-
 const app = express();
-
 const path = require('path');
 
 const port = 3030;
 
-app.use(express.static('public'));
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/', (req, res) => res.sendFile(path.resolve('./views/index.html')));
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'ejs');
 
-app.get('/login', (req, res) => res.sendFile(path.resolve('./views/login.html')))
 
-app.get('/register', (req, res) => res.sendFile(path.resolve('./views/register.html')))
+const mainRoutes = require('./routers/main');
+const usersRoutes = require('./routers/users');
 
-app.get('/productCart', (req, res) => res.sendFile(path.resolve('./views/productCart.html')))
-
-app.get('/productDetail', (req, res) => res.sendFile(path.resolve('./views/productDetail.html')))
+app.use('/', mainRoutes);
+app.use('/', usersRoutes);
 
 app.listen(port, () => console.log(`Servidor funcionando en el puerto ${port}!`));
