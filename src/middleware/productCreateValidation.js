110fa7baf8ae89a1be5c4d476881aac27a6cd126
BarrionuevoJ <julianbarrionuevo10@gmail.com>
2023-05-28 23:06:03 -0300

    Validaciones del back casi terminadas, solo falta la edicion de productos

--- src/middleware/productCreateValidation.js
+++ src/middleware/productCreateValidation.js
@@ -5,17 +5,21 @@ const productValidation = [
     body('titulo')
         .notEmpty().withMessage('El titulo es obligatorio.').bail()
         .isLength({ min: 2 }).withMessage('El titulo tiene que tener un minimo de 2 caracteres'),
-    body('autor')
-        .notEmpty().withMessage('El autor es obligatorio.').bail()
-        .isLength({ min: 3 }).withMessage('El autor tiene que tener un minimo de 3 caracteres'),
+    body('id_autor')
+        .not().equals('0').withMessage('El autor es obligatorio.').bail(),
     body('descripcion')
         .notEmpty().withMessage('La descripción es obligatoria').bail()
         .isLength({ min: 10 }).withMessage('La descripcion tiene que tener un minimo de 10 caracteres.').bail(),
-    body('categoria').isLength({ min: 1 }).withMessage('La categoria es obligatoria.').bail(),
-    body('descuento').isNumeric().default(0).withMessage('El descuento debe ser un numero.').bail(),
-    body('cantidad').isNumeric().default(1).withMessage('La cantidad debe ser un numero.').bail(),
-    body('precio').isNumeric().default(0).withMessage('El precio debe ser un numero.').bail(),
-    body('genero').isLength({ min: 1 }).withMessage('El genero es obligatorio.').bail(),
+    body('id_categoria')
+        .not().equals('0').withMessage('La categoria es obligatoria.').bail(),
+    body('descuento')
+        .isNumeric().default(0).withMessage('El descuento debe ser un numero.').bail(),
+    body('cantidad')
+        .isNumeric().default(1).withMessage('La cantidad debe ser un numero.').bail(),
+    body('precio')
+        .isNumeric().default(0).withMessage('El precio debe ser un numero.').bail(),
+    body('id_genero')
+        .not().equals('0').withMessage('El genero es obligatorio.').bail(),
     body('img').custom((value, { req }) => {
         let file = req.file;
         let acceptedExtensions = ['.jpg', '.png', '.jpeg'];
--- src/routers/products.js
+++ src/routers/products.js
@@ -12,7 +12,6 @@ const uploadFile = multerMiddleware('products', 'product');
 
 // Obtener todos los productos 
 router.get('/', productsController.all);
-router.get('/db', productsController.db);
 
 // Carrito
 router.get('/cart', authMiddleware, productsController.cart)
@@ -21,16 +20,16 @@ router.get('/cart', authMiddleware, productsController.cart)
 router.post('/addCart/:id', authMiddleware, productsController.addCart);
 
 // Crear un producto 
-router.post('/', uploadFile.single('img'), productValidation, productsController.create)
+router.post('/create', authMiddleware,uploadFile.single('img'), productValidation, productsController.create)
 
-router.get('/create', productsController.add);
+router.get('/create', authMiddleware, productsController.add);
 
 // Obtener un producto
 router.get('/detail/:id', productsController.detail);
 
 // Editar un producto
-router.get('/edit/:id', productsController.edit);
-router.put('/edit/:id', uploadFile.single('img'), productsController.update);
+router.get('/edit/:id', authMiddleware,productsController.edit);
+router.put('/edit/:id', authMiddleware,uploadFile.single('img'), productValidation, productsController.update);
 
 // Borrar un producto 
 router.delete('/delete/:id', productsController.destroy);
